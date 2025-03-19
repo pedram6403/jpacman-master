d30c0333f2103299a709cc55207b8fcbb11c0447
pedram6403 <pedram.pourmajidian@gmail.com>
2025-03-18 21:42:03 -0300

    ajuter vie

--- src/main/java/nl/tudelft/jpacman/game/Game.java
+++ src/main/java/nl/tudelft/jpacman/game/Game.java
@@ -1,6 +1,124 @@
+// package nl.tudelft.jpacman.game;
+
+// import java.util.List;
+
+// import nl.tudelft.jpacman.board.Direction;
+// import nl.tudelft.jpacman.level.Level;
+// import nl.tudelft.jpacman.level.Level.LevelObserver;
+// import nl.tudelft.jpacman.level.Player;
+// import nl.tudelft.jpacman.points.PointCalculator;
+
+// /**
+//  * A basic implementation of a Pac-Man game.
+//  *
+//  * @author Jeroen Roosen 
+//  */
+// public abstract class Game implements LevelObserver {
+
+//     /**
+//      * <code>true</code> if the game is in progress.
+//      */
+//     private boolean inProgress;
+
+//     /**
+//      * Object that locks the start and stop methods.
+//      */
+//     private final Object progressLock = new Object();
+
+//     /**
+//      * The algorithm used to calculate the points that
+//      * they player gets whenever some action happens.
+//      */
+//     private PointCalculator pointCalculator;
+
+//     /**
+//      * Creates a new game.
+//      *
+//      * @param pointCalculator
+//      *             The way to calculate points upon collisions.
+//      */
+//     protected Game(PointCalculator pointCalculator) {
+//         this.pointCalculator = pointCalculator;
+//         inProgress = false;
+//     }
+
+//     /**
+//      * Starts or resumes the game.
+//      */
+//     public void start() {
+//         synchronized (progressLock) {
+//             if (isInProgress()) {
+//                 return;
+//             }
+//             if (getLevel().isAnyPlayerAlive() && getLevel().remainingPellets() > 0) {
+//                 inProgress = true;
+//                 getLevel().addObserver(this);
+//                 getLevel().start();
+//             }
+//         }
+//     }
+
+//     /**
+//      * Pauses the game.
+//      */
+//     public void stop() {
+//         synchronized (progressLock) {
+//             if (!isInProgress()) {
+//                 return;
+//             }
+//             inProgress = false;
+//             getLevel().stop();
+//         }
+//     }
+
+//     /**
+//      * @return <code>true</code> iff the game is started and in progress.
+//      */
+//     public boolean isInProgress() {
+//         return inProgress;
+//     }
+
+//     /**
+//      * @return An immutable list of the participants of this game.
+//      */
+//     public abstract List<Player> getPlayers();
+
+//     /**
+//      * @return The level currently being played.
+//      */
+//     public abstract Level getLevel();
+
+//     /**
+//      * Moves the specified player one square in the given direction.
+//      *
+//      * @param player
+//      *            The player to move.
+//      * @param direction
+//      *            The direction to move in.
+//      */
+//     public void move(Player player, Direction direction) {
+//         if (isInProgress()) {
+//             // execute player move.
+//             getLevel().move(player, direction);
+//             pointCalculator.pacmanMoved(player, direction);
+//         }
+//     }
+
+//     @Override
+//     public void levelWon() {
+//         stop();
+//     }
+
+//     @Override
+//     public void levelLost() {
+//         stop();
+//     }
+// }
 package nl.tudelft.jpacman.game;
 
 import java.util.List;
+
+import javax.swing.JOptionPane;
 
 import nl.tudelft.jpacman.board.Direction;
 import nl.tudelft.jpacman.level.Level;
@@ -30,6 +148,11 @@ public abstract class Game implements LevelObserver {
      * they player gets whenever some action happens.
      */
     private PointCalculator pointCalculator;
+
+    /**
+     * The number of lives the player has.
+     */
+    public static int lives;
 
     /**
      * Creates a new game.
@@ -111,6 +234,39 @@ public abstract class Game implements LevelObserver {
 
     @Override
     public void levelLost() {
-        stop();
+        stop(); // Arrête le jeu temporairement
+
+        // Vérifie si au moins un joueur a des vies restantes
+        boolean hasLives = false;
+        for (Player player : getPlayers()) {
+            if (player.getLives() > 0) {
+                player.loseLife(); // Retire une vie
+                hasLives = true;
+            }
+        }
     }
+
+
+    public void restartGame() {
+        stop(); // Arrête le jeu actuel
+
+        // Réinitialise les joueurs (ne pas réinitialiser leur score ici)
+        for (Player player : getPlayers()) {
+            if (player.getLives() > 0) {
+                player.resetForNextRound(); // Méthode pour préparer le joueur à une nouvelle partie
+            }
+            if (player.getLives() == 0) {
+                //setAlive(false); // Le joueur est mort si les vies atteignent zéro
+                System.out.println("Game Over!");
+                JOptionPane.showMessageDialog(null, "Game Over!", "Game Over", JOptionPane.INFORMATION_MESSAGE);
+
+            }
+        }
+
+        // Redémarre le niveau ou le jeu
+        start(); // Relance le jeu
+    }
+
+
+
 }
